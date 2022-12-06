65ec74dbd611d0098b8c86f70776209a95bba665
Nate-K0 <nkenopic@myseneca.ca>
2022-12-05 22:23:35 -0500

    Added Add and Delete Post

--- social-media-app/src/app/home/home.component.ts
+++ social-media-app/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Post } from '../Post';
+import { PostService } from '../post.service';
 
 
 @Component({
@@ -7,11 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  posts !: Array<Post>; 
+  psts: any;
+  username !: string;
 
-  constructor() {
+  constructor(private _postService : PostService, private router : Router, private _authService : AuthService) {
   }
 
   ngOnInit(): void {
+    this._postService.getAllPosts().subscribe(post => {
+      if (post.length > 0) {
+        this.posts = post;
+      }
+    });
 
+    this.username = this._authService.getUsername()!;
+  }
+
+  handleDelete(id : string) : void {
+    this.psts = this._postService.deletePostById(id).subscribe(() => this.router.navigate(['/home']))
+  }
+
+  ngOnDestroy(): void {
+    if (this.psts) this.psts.unsubscribe();
   }
 }
