6e60daa36e55cb95e8e9ca15032d0bdc049417de
Nate-K0 <nkenopic@myseneca.ca>
2023-02-13 14:06:29 -0500

    Added Admin Posts

--- social-media-app/src/app/adminposts/adminposts.component.ts
+++ social-media-app/src/app/adminposts/adminposts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Post } from '../Post';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-adminposts',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminposts.component.css']
 })
 export class AdminpostsComponent implements OnInit {
+  allPosts: Array<Post> = [];
+  psts: any;
 
-  constructor() { }
+  constructor(private _postService: PostService, private router : Router) { }
 
   ngOnInit(): void {
+    this.psts = this._postService.getAllPosts().subscribe(post => {
+      this.allPosts = post;
+    })
   }
 
+  handleDelete(id:string) : void {
+    this.psts = this._postService.deletePostById(id).subscribe(() => this.router.navigate(['/admin/posts']))
+  }
+
+  ngOnDestroy(): void {
+    if (this.psts) this.psts.unsubscribe();
+  }
 }
